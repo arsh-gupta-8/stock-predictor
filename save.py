3af0a91f5345f97a1a31e56be3aad85c27fac2f8
arsh-gupta-8 <arshmailing@gmail.com>
2026-08-06 12:50:35 +0700

    Changed file save location for model

--- save.py
+++ save.py
@@ -1,9 +1,9 @@
 import pickle
 
 def saveModel(model, modelName="MODEL"):
-  with open(f'{modelName}_model.pkl', 'wb') as file:
+  with open(f'Model/{modelName}_model.pkl', 'wb') as file:
     pickle.dump(model, file)
 
 def loadModel(modelName="MODEL"):
-  with open(f'{modelName}_model.pkl', 'rb') as file:
+  with open(f'Model/{modelName}_model.pkl', 'rb') as file:
     return pickle.load(file)
